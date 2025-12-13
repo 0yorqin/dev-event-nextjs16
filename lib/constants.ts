00863e6f4a80db9ccb54431c4d2926f58bd2edb3
Yorqin Rahimboboyev <crossrick88@gmail.com>
2025-12-13 18:02:49 +0500

    first-commit

---
+++ lib/constants.ts
@@ -0,0 +1,52 @@
+// lconstants.ts
+
+export const events = [
+  {
+    image: "/images/event1.png",
+    title: "React Conf 2025",
+    slug: "react-conf-2025",
+    location: "Las Vegas, USA",
+    date: "April 10, 2025",
+    time: "09:00 AM",
+  },
+  {
+    image: "/images/event2.png",
+    title: "Google I/O 2025",
+    slug: "google-io-2025",
+    location: "Mountain View, USA",
+    date: "May 14, 2025",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event3.png",
+    title: "JS World Conference",
+    slug: "js-world-conference",
+    location: "Amsterdam, Netherlands",
+    date: "June 12, 2025",
+    time: "09:30 AM",
+  },
+  {
+    image: "/images/event4.png",
+    title: "Global AI Hackathon",
+    slug: "global-ai-hackathon",
+    location: "Online",
+    date: "March 22, 2025",
+    time: "12:00 PM",
+  },
+  {
+    image: "/images/event5.png",
+    title: "Next.js Conf",
+    slug: "nextjs-conf-2025",
+    location: "San Francisco, USA",
+    date: "October 9, 2025",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event6.png",
+    title: "Modern Web Dev Meetup",
+    slug: "modern-web-dev-meetup",
+    location: "Tashkent, Uzbekistan",
+    date: "February 18, 2025",
+    time: "06:30 PM",
+  },
+];
